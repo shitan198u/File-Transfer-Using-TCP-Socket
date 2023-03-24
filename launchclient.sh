6b12d9a2f85839f0f84fa8ddfe9099475a615a0a
Shitanshu Shivam <shiva.shitanshu2018@gmail.com>
2023-03-24 22:09:57 +0530

    added the .desktop files

--- launchclient.sh
+++ launchclient.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/shsrra/CODE/FFileTransfer/
 python3 /home/shsrra/CODE/FFileTransfer/client.py 
 
 
--- launchserver.sh
+++ launchserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /home/shsrra/CODE/FFileTransfer/
 python3 /home/shsrra/CODE/FFileTransfer/server.py 
 
