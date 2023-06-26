2952418e4e544d7c9d1d27a0547453b6bfa2409d
Shitanshu Shivam <shiva.shitanshu2018@gmail.com>
2023-06-26 19:54:54 +0530

    Refreshing the codebase

--- launchclient.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/shsrra/CODE/FFileTransfer/
-python3 /home/shsrra/CODE/FFileTransfer/client.py 
-
-
--- launchserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/shsrra/CODE/FFileTransfer/
-python3 /home/shsrra/CODE/FFileTransfer/server.py 
-
